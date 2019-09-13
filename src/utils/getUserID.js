f15df70483b292f0933afd739298b4ec2cc4ccb9
Saad Khan Malik <social@saadkhanmalik.com>
2019-09-13 07:17:46 +0300

    Lock Down Queries

--- src/utils/getUserID.js
+++ src/utils/getUserID.js
@@ -1,17 +1,19 @@
 import jwt from "jsonwebtoken"
 
-const getUserID = (request) => {
+const getUserID = (request, requireAuth = true) => {
   const header = request.headers.authorization
 
-  if (!header) {
+  if (header) {
+    const token = header.replace("Bearer ", "")
+    const decoded = jwt.verify(token, 'popsicle')
+    return decoded.userId
+  }
+
+  if (requireAuth) {
     throw new Error("Authentication required")
   }
 
-  const token = header.replace("Bearer ", "")
-  const decoded = jwt.verify(token, 'popsicle')
-
-  return decoded.userId
-
+  return null
 }
 
 export { getUserID as default }
